1f26a0e142f485c402cb956a8d70e2d4eebbdf8a
Nadya <nadezhdayagubova@gmail.com>
2023-02-23 13:40:25 -0500

    first commit first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/* X will go first.
+2. After X clicks, the cursor changes to O.
+3. The application should show when the win happens (through some animation or alert and state who won).
+4. Give the option to reset the game.
+5. Make it look aesthetically appealing (nice colours)
+
+ Tic Tac Toe board in HTML and CSS Flex Box
+
+
+ Tic Tac Toe board in HTML5 and CSS Flex Box
+ Animation with CSS transitions
+ Control and logic using JavaScript*/
+
+
+ 
